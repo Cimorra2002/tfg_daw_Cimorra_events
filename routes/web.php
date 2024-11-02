a18f19ff05ea2b59deb810b02d3e575d7156d74a
Cimorra2002 <diegocimorra@gmail.com>
2024-11-02 02:15:35 +0100

    login routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class,'index'])->name('login');
+Route::get('/registro', [AuthController::class,'registro'])->name('registro');
+Route::post('/registrar', [AuthController::class,'registrar'])->name('registrar');
+Route::post('/logear', [AuthController::class,'logear'])->name('logear');
+
+Route::get('/home', [AuthController::class,'home'])->name('home');
