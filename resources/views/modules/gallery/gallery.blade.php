fb29917757d4e14f3259c4726424364d2199c036
Cimorra2002 <diegocimorra@gmail.com>
2024-11-05 23:24:38 +0100

    controller and view (gallery)

---
+++ resources/views/modules/gallery/gallery.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/main')
+@section('titulo_pagina', 'Galeria')
+
+@section('navbar')
+    @include('layouts.nav.navbar')
+@endsection
+
+@section('contenido')
+    <h1>Bienvenido a la página de galeria</h1>
+@endsection
