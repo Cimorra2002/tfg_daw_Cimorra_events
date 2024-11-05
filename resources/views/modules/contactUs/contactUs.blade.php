2f0992756859e78ea1b59a9cbbc74f7c9d326959
Cimorra2002 <diegocimorra@gmail.com>
2024-11-05 20:50:06 +0100

    controller and view (contactUs)

---
+++ resources/views/modules/contactUs/contactUs.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/main')
+@section('titulo_pagina', 'Contactanos')
+
+@section('navbar')
+    @include('layouts.nav.navbar')
+@endsection
+
+@section('contenido')
+    <h1>Bienvenido a la página de contactanos</h1>
+@endsection
