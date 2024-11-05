adc70bfd93b44e79fc25c2ad2d38b01f0193ad89
Cimorra2002 <diegocimorra@gmail.com>
2024-11-05 22:32:30 +0100

    controller and view (events)

---
+++ resources/views/modules/events/events.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/main')
+@section('titulo_pagina', 'Eventos')
+
+@section('navbar')
+    @include('layouts.nav.navbar')
+@endsection
+
+@section('contenido')
+    <h1>Bienvenido a la página de eventos</h1>
+@endsection
