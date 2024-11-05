c44563a2a19f3daf0dd6f0b9ed9699ff9681f900
Cimorra2002 <diegocimorra@gmail.com>
2024-11-05 19:38:43 +0100

    create Controler and view (AboutUs)

---
+++ resources/views/modules/aboutUs/aboutUs.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/main')
+@section('titulo_pagina', 'Sobre nosotros')
+
+@section('navbar')
+    @include('layouts.nav.navbar')
+@endsection
+
+@section('contenido')
+    <h1>Bienvenido a la página de sobre nosotros</h1>
+@endsection
