3591755bff8ad76c02855e0ab613f305b738bec4
Cimorra2002 <diegocimorra@gmail.com>
2024-11-06 22:08:11 +0100

    añado campo rol a User con su migracion

---
+++ database/migrations/2024_11_06_184250_add_role_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('user'); // Agrega el campo 'role' con valor por defecto 'user'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role'); // Elimina el campo 'role'
+        });
+    }
+};
