d3e7cdb48110d94125f67ee20b6d0cc318cf1ba6
Cimorra2002 <diegocimorra@gmail.com>
2024-11-08 17:53:46 +0100

    link icons bootstrap and link style contactUs

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="{{ asset('css/contactUs.css') }}" rel="stylesheet">
     <title>@yield('titulo_pagina')</title>
   </head>
   <body>
