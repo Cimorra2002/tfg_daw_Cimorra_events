05d87b5c2047fbc3f3eaff38f06e80ca5a0db777
Cimorra2002 <diegocimorra@gmail.com>
2024-11-12 22:41:02 +0100

    vistas fiestas

---
+++ resources/views/modules/gallery/bloody.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+
+@section('titulo_pagina', 'Galería Bloody')
+
+@section('contenido')
+<div class="container my-5">
+    <h2 class="text-center mb-4">Galería de Que Resacón en Bloody</h2>
+    <div class="row">
+        @foreach($imagenesBloody as $imagen)
+            <div class="col-lg-3 col-md-4 mb-4">
+                <div class="gallery-item">
+                    <a href="{{ url($imagen) }}" class="glightbox" data-gallery="images-gallery">
+                        <img src="{{ url($imagen) }}" alt="Imagen de Bloody" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <div class="text-center mt-4">
+        <a href="{{ url()->previous() }}" class="btn btn-secondary">Volver</a>
+    </div>
+</div>
+@endsection
