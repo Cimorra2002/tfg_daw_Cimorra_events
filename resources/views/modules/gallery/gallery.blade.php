ce8dd667b3969f78edb88e92f456f41eefb42abe
Cimorra2002 <diegocimorra@gmail.com>
2024-11-12 18:29:54 +0100

    view gallery (estructura)

--- resources/views/modules/gallery/gallery.blade.php
+++ resources/views/modules/gallery/gallery.blade.php
@@ -6,5 +6,62 @@
 @endsection
 
 @section('contenido')
-    <h1>Bienvenido a la página de galeria</h1>
+<section id="about" class="gallery section light-background pt-5 mt-5 pb-5">
+    <div class="container">
+        <div class="row align-items-xl-center gy-5 px-5">
+            <h2 class="text-center">Sobre nosotros</h2>
+        </div>
+        <div class="container d-flex flex-column align-items-center text-center" data-aos="fade-up" data-aos-delay="100">
+            <div class="party row g-0 mt-5">
+                <h3>Que Resacón en Moogli - 1 de Marzo de 2024</h3>
+                <div class="col-lg-3 col-md-4">
+                    <div class="gallery-item">
+                        <a href="{{ url('images/fiesta2/IMG_11.jpg') }}" class="glightbox" data-gallery="images-gallery">
+                            <img src="{{ url('images/fiesta2/IMG_11.jpg') }}" alt="" class="img-fluid">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="gallery-item">
+                        <a href="{{ url('images/fiesta2/IMG_4.jpg') }}" class="glightbox" data-gallery="images-gallery">
+                            <img src="{{ url('images/fiesta2/IMG_4.jpg') }}" alt="" class="img-fluid">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="gallery-item">
+                        <a href="{{ asset('images/fiesta2/IMG_7.jpg') }}" class="glightbox" data-gallery="images-gallery">
+                            <img src="{{ asset('images/fiesta2/IMG_7.jpg') }}" alt="" class="img-fluid">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container d-flex flex-column align-items-center text-center" data-aos="fade-up" data-aos-delay="100">
+            <div class="party row g-0 mt-5">
+                <h3>Que Resacón en Moogli - 1 de Marzo de 2024</h3>
+                <div class="col-lg-3 col-md-4">
+                    <div class="gallery-item">
+                        <a href="{{ url('images/fiesta2/IMG_11.jpg') }}" class="glightbox" data-gallery="images-gallery">
+                            <img src="{{ url('images/fiesta2/IMG_11.jpg') }}" alt="" class="img-fluid">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="gallery-item">
+                        <a href="{{ url('images/fiesta2/IMG_4.jpg') }}" class="glightbox" data-gallery="images-gallery">
+                            <img src="{{ url('images/fiesta2/IMG_4.jpg') }}" alt="" class="img-fluid">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="gallery-item">
+                        <a href="{{ asset('images/fiesta2/IMG_7.jpg') }}" class="glightbox" data-gallery="images-gallery">
+                            <img src="{{ asset('images/fiesta2/IMG_7.jpg') }}" alt="" class="img-fluid">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
