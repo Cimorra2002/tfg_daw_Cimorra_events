99ab15448ec57956bddc49262d8ab21295ba5325
Cimorra2002 <diegocimorra@gmail.com>
2024-11-16 22:22:02 +0100

    change view events and create view maintenance

--- resources/views/modules/events/events.blade.php
+++ resources/views/modules/events/events.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('contenido')
-    <section id="contact" class="contact section pt-5 mt-5 d-flex" style="min-height: 80vh;">
+    <section class="pt-5 mt-5 d-flex" style="min-height: 80vh;">
         <div class="container text-center">
             <h2 class="text-center mb-5">Eventos</h2>
 
@@ -24,22 +24,22 @@
                         </a>
                     </div>
                     <div class="col-md-4 mb-3">
-                        <a href="#" class="btn btn-primary btn-xxl w-100">
+                        <a href="{{ url('/maintenance') }}" class="btn btn-primary btn-xxl w-100">
                             EJEA
                         </a>
                     </div>
                     <div class="col-md-4 mb-3">
-                        <a href="#" class="btn btn-primary btn-xxl w-100">
+                        <a href="{{ url('/maintenance') }}" class="btn btn-primary btn-xxl w-100">
                             HUESCA
                         </a>
                     </div>
                     <div class="col-md-4 mb-3">
-                        <a href="#" class="btn btn-primary btn-xxl w-100">
+                        <a href="{{ url('/maintenance') }}" class="btn btn-primary btn-xxl w-100">
                             TERUEL
                         </a>
                     </div>
                     <div class="col-md-4 mb-3">
-                        <a href="#" class="btn btn-primary btn-xxl w-100">
+                        <a href="{{ url('/maintenance') }}" class="btn btn-primary btn-xxl w-100">
                             GRAÑEN
                         </a>
                     </div>
@@ -48,18 +48,3 @@
         </div>
     </section>
 @endsection
-
-<!--
-
-<div class="col-lg-6 col-md-6">
-                <div class="evento text-start">
-                    <h2 class="titulo">Que resacon</h2>
-                    <div class="products-content">
-                        <h4 class="fecha">14/12/2024</h4>
-                        <h4 class="horas">23:00 - 05:30</h4>
-                        <h4 class="ubicacion">Tarazona</h4>
-                        <h4 class="precio">14 €</h4>
-                        <h4 class="descripcion">esta fiesta trata.....</h4>
-                    </div>
-                </div>
-            </div>-->
---
+++ resources/views/modules/events/maintenance.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts/main')
+@section('titulo_pagina', 'Eventos')
+
+@section('navbar')
+    @include('layouts.nav.navbar')
+@endsection
+
+@section('contenido')
+
+    <section class="pt-5 mt-5 d-flex">
+        <div class="container text-center">
+            <h2 class="text-center mb-3">Pronto llegaremos a esta zona.</h2>
+            <h3 class="mb-5">Disfruta de nuestras fiestas exclusivas en otros pueblos o ciudades.</h3>
+            <div class="maintenance-container">
+                <div>
+                    <img src="{{ url('gifs/dance.gif') }}" class="img-fluid custom-gif" alt="Mantenimiento en progreso">
+                </div>
+            </div>
+            <a href="{{ url('/events') }}" class="btn btn-home mt-3">Volver a eventos</a>
+        </div>
+    </section>
+
+@endsection
