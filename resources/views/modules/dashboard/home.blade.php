1b951ead691632a13beb5cd87ebb73a843ca05c7
Cimorra2002 <diegocimorra@gmail.com>
2024-11-16 22:23:43 +0100

    changes in view home

--- resources/views/modules/dashboard/home.blade.php
+++ resources/views/modules/dashboard/home.blade.php
@@ -6,9 +6,26 @@
 @endsection
 
 @section('contenido')
-    <section id="contact" class="contact section pt-5 mt-5" >
-        <div class="container section-title">
-            <h1 class="text-center">CIMORRA EVENTS</h1>
+<section class="pt-5 mt-5">
+    <div class="container section-title">
+        <h1 class="text-center mb-5">CIMORRA EVENTS</h1>
+        <div class="search-bar w-100 mt-5 mb-5 mb-md-0">
+            <form action="">
+                <input class="py-2 ps-5" type="text" placeholder="Busqueda...">
+                <a class="search-icon" href=""><i class="bi bi-search"></i></a>
+            </form>
         </div>
-    </section>
+        <div class="event-card " style="margin-top: 15%">
+            <h4 class="text-center mb-2 mt-2">Nuestro próximo evento</h4>
+            <a href="{{ url('/events/2/1') }}">
+                <div class="w-100 event-image"
+                    style="background-image: url({{ url('images/fiesta2/IMG_13.jpg') }});"></div>
+                <div class="event-info p-3">
+                    <h4 class="mb-1">evento_nombre</h4>
+                    <span class="text-decoration-none"> Hola</span>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
 @endsection
