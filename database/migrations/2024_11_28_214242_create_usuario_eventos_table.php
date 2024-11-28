439994d7bf6e78e7c669aa52c421d538f4dd9990
Cimorra2002 <diegocimorra@gmail.com>
2024-11-28 23:35:21 +0100

    creacion de tabla intermedia (users_events)

---
+++ database/migrations/2024_11_28_214242_create_usuario_eventos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuario_eventos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('usu_id');
+            $table->unsignedBigInteger('evento_id');
+
+            // Definir las claves foráneas correctamente
+            $table->foreign('usu_id')->references('usu_id')->on('users')->onDelete('cascade');
+            $table->foreign('evento_id')->references('evento_id')->on('eventos')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuario_eventos');
+    }
+};
