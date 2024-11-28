40dde8f408440949e71e93ab429f353c298b2a6e
Cimorra2002 <diegocimorra@gmail.com>
2024-11-28 23:36:26 +0100

    validar si se crea un evento

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Evento;
 use Illuminate\Http\Request;
 
 class EventsController extends Controller
@@ -21,6 +22,22 @@ class EventsController extends Controller
 
     public function showEvent($cityId, $eventId) {
         return view("modules/events/showEvent");
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'evento_nombre' => 'required|string|max:100',
+            'evento_fecha' => 'required|date',
+            'evento_hora_inicio' => 'required|date_format:H:i',
+            'evento_hora_fin' => 'required|date_format:H:i|after:evento_hora_inicio',
+            'evento_precio' => 'nullable|numeric|min:0',
+            'evento_descripcion' => 'nullable|string',
+            'localiz_id' => 'required|exists:localizaciones,localiz_id',
+        ]);
+
+        Evento::create($request->all());
+        return redirect()->route('eventos.index')->with('success', 'Evento creado exitosamente.');
     }
 
 }
