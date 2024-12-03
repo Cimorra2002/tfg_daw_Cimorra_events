8c64099b498d7e00f08fb4768155967a596a65c1
Cimorra2002 <diegocimorra@gmail.com>
2024-12-03 21:24:53 +0100

    eliminar filtros en cityEvents

--- resources/views/modules/events/cityEvents.blade.php
+++ resources/views/modules/events/cityEvents.blade.php
@@ -10,29 +10,10 @@
         <div class="container text-center">
             <h2 class="text-center mb-5 mt-5">Eventos en {{ $localizacion->localiz_nombre }}</h2>
 
-            <div class="container mb-5 w-100 gap-3">
-                <div class="d-block d-md-flex flex-row justify-content-between w-100">
-                    <div class="search-bar w-100 mb-3 mb-md-0">
-                        <form action="">
-                            <input class="py-2 ps-5" type="text" placeholder="Busqueda...">
-                            <a class="search-icon" href=""><i class="bi bi-search"></i></a>
-                        </form>
-                    </div>
-
-
-                    <div class="d-flex flex-row justify-content-around w-100">
-                        <div class="filter-container px-5 py-1 d-flex align-items-center">filtro</div>
-                        <div class="filter-container px-5 py-1 d-flex align-items-center active">filtro</div>
-                        <div class="filter-container px-5 py-1 d-flex align-items-center">filtro</div>
-                    </div>
-                </div>
-            </div>
-
             <!-- Event List -->
             <div class="container-fluid text-center">
-                <div class="results w-100 mb-3"><span>{{ $eventos->count() }} Resultados</span></div>
+                <div class="results w-100 mb-5"><span>{{ $eventos->count() }} Resultados</span></div>
                 <div class="row display-flex justify-content-around">
-
                     <!-- Card -->
                     @foreach($eventos as $evento)
                         <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-5 event-card">
