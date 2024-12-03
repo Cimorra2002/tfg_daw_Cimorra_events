3fc5e10a85651c6af56418aca03315a3d50bc3f3
Cimorra2002 <diegocimorra@gmail.com>
2024-12-03 20:55:12 +0100

    desaharcodear

--- resources/views/modules/events/events.blade.php
+++ resources/views/modules/events/events.blade.php
@@ -8,41 +8,34 @@
 @section('contenido')
     <section class="pt-5 mt-5 d-flex" style="min-height: 80vh;">
         <div class="container text-center">
+            @if (Auth::user()->role === 'admin')
+                <div class="d-flex m-4 gap-4 admin-buttons">
+                    <a class="btn btn-success" href="{{ url('/createEvent') }}">Crear evento</a>
+                    <a class="btn btn-warning" href="{{ url('/menuEvent') }}">Editar evento</a>
+                    <a class="btn btn-danger" href="{{ url('/menuEvent') }}">Eliminar evento</a>
+                </div>
+            @endif
             <h2 class="text-center mb-5">Eventos</h2>
 
             <!-- Contenedor de botones centrados -->
-            <div class="d-flex flex-column justify-content-center align-items-center">
+             <!-- Contenedor de botones centrados -->
+             <div class="d-flex flex-column justify-content-center align-items-center">
                 <div class="row justify-content-center mb-4">
-                    <div class="col-md-4 mb-3">
-                        <a href="{{ url('/events/1') }}" class="btn btn-primary btn-xxl w-100">
-                            ZARAGOZA
-                        </a>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <a href="#" class="btn btn-primary btn-xxl w-100">
-                            TARAZONA
-                        </a>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <a href="{{ url('/maintenance') }}" class="btn btn-primary btn-xxl w-100">
-                            EJEA
-                        </a>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <a href="{{ url('/maintenance') }}" class="btn btn-primary btn-xxl w-100">
-                            HUESCA
-                        </a>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <a href="{{ url('/maintenance') }}" class="btn btn-primary btn-xxl w-100">
-                            TERUEL
-                        </a>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <a href="{{ url('/maintenance') }}" class="btn btn-primary btn-xxl w-100">
-                            GRAÑEN
-                        </a>
-                    </div>
+                    @foreach($localizacionesConEventos as $localizacion)
+                        <div class="col-md-4 mb-3">
+                            @if ($localizacion->has_events)
+                                <!-- Si hay eventos, muestra un enlace a los eventos de la ciudad -->
+                                <a href="{{ url('/events/' . $localizacion->localiz_id) }}" class="btn btn-primary btn-xxl w-100">
+                                    {{ $localizacion->localiz_nombre }}
+                                </a>
+                            @else
+                                <!-- Si no hay eventos, muestra un enlace a la vista de mantenimiento -->
+                                <a href="{{ url('/maintenance') }}" class="btn btn-secondary btn-xxl w-100">
+                                    {{ $localizacion->localiz_nombre }}
+                                </a>
+                            @endif
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
--- resources/views/modules/events/showEvent.blade.php
+++ resources/views/modules/events/showEvent.blade.php
@@ -7,20 +7,14 @@
 
 @section('contenido')
     <div class="event-title d-flex justify-content-center align-items-center"
-        style="background-image: url({{ url('images/fiesta2/IMG_1.jpg') }});">
-        <h2>EVENTO_NOMBRE</h2>
+        style="background-image: url({{ url('storage/' . $evento->evento_imagen) }});">
+        <h2>{{ $evento->evento_nombre }}</h2>
     </div>
+
     <div class="p-5 event-details">
         <section>
             <h4 class="mb-3">Descripción</h4>
-            <p>evento-descripcion evento-descripcion evento-descripcion evento-descripcion evento-descripcion
-                evento-descripcion evento-descripcion evento-descripcion evento-descripcion evento-descripcion
-                evento-descripcion evento-descripcion evento-descripcion evento-descripcion evento-descripcion
-                evento-descripcion evento-descripcion evento-descripcion evento-descripcion evento-descripcion
-                evento-descripcion evento-descripcion evento-descripcion evento-descripcion evento-descripcion
-                evento-descripcion evento-descripcion evento-descripcion evento-descripcion evento-descripcion
-                evento-descripcion evento-descripcion evento-descripcion evento-descripcion evento-descripcion
-                evento-descripcion </p>
+            <p>{{ $evento->evento_descripcion }}</p>
         </section>
 
         <section class="py-4">
@@ -28,19 +22,40 @@
             <div class="d-flex flex-row justify-content-between w-100">
                 <div class="d-flex flex-column">
                     <span class="details-text">FECHA</span>
-                    <span>evento_fecha</span>
+                    <span>
+                        @if ($evento->evento_fecha)
+                            {{ \Carbon\Carbon::parse($evento->evento_fecha)->format('d/m/Y') }}
+                        @else
+                            No disponible
+                        @endif
+                    </span>
                 </div>
+
                 <div class="d-flex flex-column">
                     <span class="details-text">HORA INICIO</span>
-                    <span>evento_hora_inicio</span>
+                    <span>
+                        @if ($evento->evento_hora_inicio)
+                            {{ \Carbon\Carbon::parse($evento->evento_hora_inicio)->format('H:i') }}
+                        @else
+                            No disponible
+                        @endif
+                    </span>
                 </div>
+
                 <div class="d-flex flex-column">
                     <span class="details-text">HORA FIN</span>
-                    <span>evento_hora_fin</span>
+                    <span>
+                        @if ($evento->evento_hora_fin)
+                            {{ \Carbon\Carbon::parse($evento->evento_hora_fin)->format('H:i') }}
+                        @else
+                            No disponible
+                        @endif
+                    </span>
                 </div>
+
                 <div class="d-flex flex-column">
                     <span class="details-text">LOCALIDAD</span>
-                    <span>evento_hora_localidad</span>
+                    <span>{{ $localizacion->localiz_nombre }}</span>
                 </div>
             </div>
         </section>
@@ -51,17 +66,19 @@
                 <div class="d-flex flex-column">
                     <span class="details-text">Precio</span>
                     <div class="d-flex gap-5 align-items-center">
-                        <span class="price-text">15,00€</span>
+                        <span class="price-text">{{ number_format($evento->evento_precio, 2) }}€</span>
                         <span>hay existencias</span>
                     </div>
                 </div>
             </div>
             <div>
-                <a class="d-flex justify-content-between align-items-center gap-3 px-4 py-2 button-purchease" href="">
+                <a class="d-flex justify-content-between align-items-center gap-3 px-4 py-2 button-purchease"
+                    href="">
                     <i class="bi bi-bag"></i>
                     <span>Comprar entrada</span>
                 </a>
             </div>
         </section>
     </div>
+
 @endsection
