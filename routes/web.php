170033eebea13b0167638ea8415514e6a43d26b7
Cimorra2002 <diegocimorra@gmail.com>
2024-12-03 20:52:47 +0100

    rutas

--- routes/web.php
+++ routes/web.php
@@ -22,20 +22,29 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/logout', [AuthController::class,'logout'])->name('logout');
     Route::get('/home', [AuthController::class,'home'])->name('home');
     Route::get('/home', [HomeController::class,'home'])->name('home');
+
     Route::get('/events', [EventsController::class,'events'])->name('events');
-    Route::get('/events/{id}', [EventsController::class,'cityEvents'])->name('cityEvents');
-    Route::get('/events/{id}/{event}', [EventsController::class,'showEvent'])->name('showEvent');
+    Route::get('/events/{localiz_id}', [EventsController::class, 'showCityEvents']);
+    Route::get('/events/{localiz_id}/{evento_id}', [EventsController::class, 'showEventDetails']);
     Route::get('/maintenance', [EventsController::class,'maintenance'])->name('maintenance');
+
     Route::get('/gallery', [GalleryController::class,'gallery'])->name('gallery');
-    Route::get('/contactUs', [ContactUsController::class,'contactUs'])->name('contactUs');
-    Route::post('/contactUs', [ContactUsController::class,'store'])->name('contactUs.store');
-    Route::get('/aboutUs', [AboutUsController::class,'aboutUs'])->name('aboutUs');
     Route::get('/gallery/moogli', [GalleryController::class, 'showMoogli'])->name('gallery.moogli');
     Route::get('/gallery/bloody', [GalleryController::class, 'showBloody'])->name('gallery.bloody');
+
+    Route::get('/contactUs', [ContactUsController::class,'contactUs'])->name('contactUs');
+    Route::post('/contactUs', [ContactUsController::class,'store'])->name('contactUs.store');
+
+    Route::get('/aboutUs', [AboutUsController::class,'aboutUs'])->name('aboutUs');
 });
 
 // Rutas solo para administradores
 Route::middleware(['auth', IsAdmin::class])->group(function () {
     Route::get('/createEvent', [EventsController::class,'addEvent'])->name('createEvent');
     Route::post('/createEvent', [EventsController::class,'store'])->name('createEvent.store');
+    Route::get('/menuEvent', [EventsController::class,'menuEvent'])->name('menuEvent');
+    Route::get('/menuEvent', [EventsController::class,'showEventsLocalization'])->name('menuEvent');
+    Route::get('/editEvent/{id}', [EventsController::class, 'edit'])->name('editEvent');
+    Route::put('/updateEvent/{id}', [EventsController::class, 'update'])->name('updateEvent');
+    Route::delete('/evento/{id}', [EventsController::class, 'destroy'])->name('deleteEvent');
 });
